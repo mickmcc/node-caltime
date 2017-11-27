6846253c1ec8db60e2d3c19981ca2f4ec8196a7b
Michael McCarthy <michael.mccarthy@ieee.org>
2017-11-27 11:18:20 +0100

    Add script to convert SVG images to PNG. Update Readme doc images.

---
+++ docs/scripts/convertimg.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+convert -background none -size 600x400 ../docs/img-src/difference_1.svg ../docs/img/difference_1.png
+convert -background none -size 600x400 ../docs/img-src/union_1.svg ../docs/img/union_1.png
+convert -background none -size 600x400 ../docs/img-src/intersect_1.svg ../docs/img/intersect_1.png
+convert -background none -size 600x400 ../docs/img-src/subtract_1.svg ../docs/img/subtract_1.png
